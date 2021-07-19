f0383da312baab937124673835101ebabf748798
ismaelgarridomunoz@gmail.com <Ismael Garrido Muoz>
2021-07-19 14:53:35 +0200

    Generar TSV con pares M/F

---
+++ fuentes/generateGender.py
@@ -0,0 +1,45 @@
+import os
+import re
+words = []
+for file in os.listdir():
+    if file.endswith(".txt") and not file.startswith('dict'):
+        file = open(file, "r", encoding="utf-8")
+        for line in file:
+            words.append(line.rstrip())
+cleanWords = sorted(set([re.sub('[0-9- ]', '', word) for word in words]))
+withComma = [word for word in cleanWords if ',' in word]
+
+genderized = []
+for word in withComma:
+    male = re.search(r'(\w*),(\w*)',word)[1]
+    suffix = re.search(r'(\w*),(\w*)',word)[2]
+    try:
+        if suffix != 'a':
+            bondingPoint = abs(re.search(suffix[0], male[::-1]).start()-len(male)+1)
+            prefix = male[:bondingPoint]
+            female = prefix+suffix
+            genderized.append(male + "\t" + female)
+			#genderized.append(male)
+            #genderized.append(female)
+            #print('%s - %s' % (male, suffix))
+            #print(female)
+        else:
+            if male.endswith('o'):
+                female = male[:-1] + suffix
+                genderized.append(male + "\t" + female)
+                #genderized.append(male)
+                #genderized.append(female)
+                #print('%s - %s' % (male, suffix))
+                #print(female)
+            else:
+            	female = male + suffix
+            	genderized.append(male + "\t" + female)
+                #genderized.append(male)
+                #genderized.append(female)
+                #print('%s - %s' % (male, suffix))
+                #print(female)
+    except:
+        print(male)
+words = sorted(set(genderized))
+with open('../rae-generos.txt', mode='wt', encoding='utf-8') as file:
+    file.write('\n'.join(words))
